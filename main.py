654a66c018af8ea83d64201515cf0351acd5b51f
mosesmdev <moses.morales@se.com>
2025-02-17 14:04:24 -0600

    player movement, asteroids, collosion detection

--- main.py
+++ main.py
@@ -3,19 +3,44 @@
 # throughout this file
 import pygame
 from constants import *
+from asteroidfield import *
+from player import Player
+from asteroid import Asteroid
 
 def main():
     pygame.init()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     running = True
+    playerx = SCREEN_WIDTH / 2
+    playery = SCREEN_HEIGHT / 2
+    player = Player(playerx, playery)
+    asteroidfield = AsteroidField()
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
         
+        screen.fill((0, 0, 0))
+        updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collision(player):
+                print("Game Over!")
+                running = False
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
     pygame.quit()
-    
+
 if __name__ == "__main__":
     main()
