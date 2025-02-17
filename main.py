a56e662a3e78fcedc00044438a0925d11904f137
mosesmdev <moses.morales@se.com>
2025-02-17 12:29:44 -0600

    screen created

---
+++ main.py
@@ -0,0 +1,21 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        
+        pygame.display.flip()
+
+    pygame.quit()
+    
+if __name__ == "__main__":
+    main()
